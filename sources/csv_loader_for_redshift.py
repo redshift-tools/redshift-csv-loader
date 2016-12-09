0f477e8b78a5d92676654fa2236efc694d40198e
Alex Buzunov <alexbuzunov@gmail.com>
2016-12-09 13:24:46 -0500

    Update csv_loader_for_redshift.py

--- sources/csv_loader_for_redshift.py
+++ sources/csv_loader_for_redshift.py
@@ -1,6 +1,6 @@
 """#############################################################################
 #CSV-to-Redshift Data Loader (v1.2, beta, 04/05/2016 15:11:53) [64bit] 
-#Copyright (c): Free to change or distribute.
+#Copyright (c): Free to change, modify or distribute (star this repo).
 #Agreement: Use this tool at your own risk. Author is not liable for any damages 
 #           or losses related to the use of this software.
 ################################################################################
